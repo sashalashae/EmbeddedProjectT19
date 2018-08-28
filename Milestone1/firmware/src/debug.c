849484a57de81b66439b637280bdf2ba81f9736c
Alex Nolan <ajn96@vt.edu>
2018-08-27 22:59:42 -0400

    Added template files for all extra data structures needed for milestone 1

--- Milestone1/firmware/src/debug.c
+++ Milestone1/firmware/src/debug.c
@@ -1,7 +1,6 @@
 /* ************************************************************************** */
-// Filename:    sensor_state.c 
+// Filename:    debug.c 
 // Date:        8/27/2018 
 // Author:
-// Description: Implements a state machine to read sensor data from the ADC and 
-//              average that data over five polling cycles.
+// Description: 
 /* ************************************************************************** */
