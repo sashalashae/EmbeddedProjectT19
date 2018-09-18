4a6726fa84762f76d7a64b95146925d973a81247
Alex Nolan <ajn96@vt.edu>
2018-09-17 21:40:38 -0400

    Added code to configure other 3 PWM modules

--- Milestone2/RobotArm/RobotArm/firmware/src/arm_library.c
+++ Milestone2/RobotArm/RobotArm/firmware/src/arm_library.c
@@ -10,25 +10,42 @@
 
 void initializeArmControl()
 {
-    //Initially disable OC1
+    //Initially disable all output compare modules
     OC1CON = 0;
-    //OC1RS / PR2 = Duty Cycle
+    OC2CON = 0;
+    OC3CON = 0;
+    
+    //OCxRS / PR2 = Duty Cycle
     //Positive pulse needs to be from 1ms (-90 degrees) to 2m (90 degrees)
-    //OC1RS / PR2 = 1.5ms/20ms -> OC1RS = 1875
+    //OCxRS / PR2 = 1.5ms/20ms -> OCxRS = 1875
     OC1RS = 1875;
-    //Initialize OC1R before enabling compare module
+    OC2RS = 1875;
+    OC3RS = 1875;
+    
+    //Initialize OCxR before enabling compare module
     OC1R = 1875;
-    //Set output compare module 1 to PWM mode (fault pin disabled)
+    OC2R = 1875;
+    OC3R = 1875;
+    
+    //Set output compare modules to PWM mode (fault pin disabled)
     OC1CON = 6;
+    OC2CON = 6;
+    OC3CON = 6;
+    
     //PR = Timer Source Clock / (PWM Frequency * Prescaler) - 1
     //PR = 80000000Hz / (64 Prescaler * 50 Hz) - 1 = 24999
     PR2 = 24999;
+    
     //Set timer 2 prescaler to 64
     T2CON = 0x60;
+    
     //Enable timer 2
-    T2CONSET = 0x8000;
-    //Enable output compare 1
-    OC1CONSET = 0x8000;
+    T2CONSET = BIT15;
+    
+    //Enable output compare modu;es
+    OC1CONSET = BIT15;
+    OC2CONSET = BIT15;
+    OC3CONSET = BIT15;
 }
 
 void setServoAngle(uint8_t compareModule, int16_t servoAngle)
