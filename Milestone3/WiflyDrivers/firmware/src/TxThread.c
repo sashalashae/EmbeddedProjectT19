a37ed2dbdc5fce1a28d310f485b0ec027ce4d37a
Alex Nolan <ajn96@vt.edu>
2018-10-07 11:30:15 -0400

    Added txthread implementation

--- Milestone3/WiflyDrivers/firmware/src/TxThread.c
+++ Milestone3/WiflyDrivers/firmware/src/TxThread.c
@@ -55,15 +55,32 @@ void TXTHREAD_Initialize ( void )
 
 void TXTHREAD_Tasks ( void )
 {
+    int index, count;
     cJSON msg;
+    uint8_t* msgPtr;
+    uint8_t currentByte;
     while(1)
     {
         //receive a JSON message to transmit
         msg = TxThreadQueue_Receive();
+        //get pointer to the message
+        msgPtr = (uint8_t *)&msg;
+        //set count to 0
+        count = 0;
         
-        //begin filling the TxISRQueue
-        
-        //Enable TX interrupts
+        //begin filling the TxISRQueue in increments of 8 bytes
+        while(count < sizeof(msg))
+        {
+            for(index = 0; index<8; index++)
+            {
+                currentByte = *msgPtr;
+                TxISRQueue_Send(currentByte);
+                msgPtr++;
+                count++;
+            }
+            //Enable TX interrupts
+            SYS_INT_SourceEnable(INT_SOURCE_USART_1_TRANSMIT);
+        }
     }
 }
 
