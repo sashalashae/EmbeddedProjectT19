e0bbb8b1958b2f808c50fd256148b345efbb5051
Thomas <tbellerose96@gmail.com>
2018-10-07 15:06:43 -0400

    Added UART_Defines.h

---
+++ Milestone3/WiflyDrivers/firmware/src/UART_Defines.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   UART_Defines.h
+ * Author: Thomas
+ *
+ * Created on October 7, 2018, 2:51 PM
+ */
+
+#ifndef UART_DEFINES_H
+#define	UART_DEFINES_H
+
+#define MAX_MESSAGE_SIZE 512
+
+#endif	/* UART_DEFINES_H */
+
