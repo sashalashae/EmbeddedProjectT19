6e27abd0b3fd2803e6470b8a08bd474f248f70b8
ajn96 <ajn96@vt.edu>
2018-11-02 14:46:58 -0900

    Finalized server for arm milestone 4 demo

--- Milestone4/RobotArm/server/arm_server.py
+++ Milestone4/RobotArm/server/arm_server.py
@@ -6,35 +6,50 @@ from json import loads
 
 currentMove = "none"
 commandNum = 0
+picbusy = False
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
 	global currentMove
 	global commandNum
+	global picbusy
         self.send_response(200)
         self.end_headers()
         message = threading.currentThread().getName()
         data = str(self.rfile.read(int(self.headers.getheader('Content-Length'))))
 	print("GET received")
 	#if request is PIC asking for move
+	if data[:7] == "getmove":
+		print("Sending move to PIC - " + currentMove)
+		self.wfile.write("{" + currentMove + "}")
+		if currentMove != "none":
+			currentMove = "none"
+			picbusy = True
+		else:
+			picbusy = False
+	else:
+		print("ERROR: Invalid GET received")
         return
 
     def do_POST(self):
 	global currentMove
 	global commandNum
+	global picbusy
         self.send_response(200)
         self.end_headers()
         data = str(self.rfile.read(int(self.headers.getheader('Content-Length'))))
 	print("POST received")
 	#draw X command
 	if data == "drawX":
-		print("Draw O command received")
+		print("Draw X command received")
 		commandNum = commandNum + 1
+		currentMove = "drawX"
 		self.wfile.write(str(commandNum))
 	#draw O command
 	elif data == "drawO":
-		print("Draw X command received")
+		print("Draw O command received")
 		commandNum = commandNum + 1
+		currentMove = "drawO"
 		self.wfile.write(str(commandNum))
 	#check status
 	elif data == "checkConnection":
@@ -46,6 +61,10 @@ class Handler(BaseHTTPRequestHandler):
 		print("Command Number Reset Received")
 		commandNum = 0
 		self.wfile.write(str(commandNum))
+	elif data == "getpicbusy":
+		commandNum = commandNum + 1
+		print("PIC Busy status command received")
+		self.wfile.write(str(picbusy))
 	else:
 		print("Error: Invalid POST body")
         return
