80cf972061027f171fdc0933e24b22bca0a26e68
ajn96 <ajn96@vt.edu>
2018-11-02 12:55:35 -0900

    Added server code

--- Milestone4/RobotArm/server/arm_server.py
+++ Milestone4/RobotArm/server/arm_server.py
@@ -4,24 +4,50 @@ import threading
 import json
 from json import loads
 
+currentMove = "none"
+commandNum = 0
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.close_connection = 0
+	global currentMove
+	global commandNum
         self.send_response(200)
         self.end_headers()
         message = threading.currentThread().getName()
         data = str(self.rfile.read(int(self.headers.getheader('Content-Length'))))
-        jsondata = json.loads(data)
-	print("GET processed")
+	print("GET received")
+	#if request is PIC asking for move
         return
 
     def do_POST(self):
-        self.close_connection = 0
+	global currentMove
+	global commandNum
         self.send_response(200)
         self.end_headers()
-        rawdata = str(self.rfile.read(int(self.headers.getheader('Content-Length'))))
-	print(rawdata)
-	print("POST processed")
+        data = str(self.rfile.read(int(self.headers.getheader('Content-Length'))))
+	print("POST received")
+	#draw X command
+	if data == "drawX":
+		print("Draw O command received")
+		commandNum = commandNum + 1
+		self.wfile.write(str(commandNum))
+	#draw O command
+	elif data == "drawO":
+		print("Draw X command received")
+		commandNum = commandNum + 1
+		self.wfile.write(str(commandNum))
+	#check status
+	elif data == "checkConnection":
+		print("Connection check command received")
+		commandNum = commandNum + 1
+		self.wfile.write("Good Connection - Command " + str(commandNum))
+	#reset command number
+	elif data == "resetNumber":
+		print("Command Number Reset Received")
+		commandNum = 0
+		self.wfile.write(str(commandNum))
+	else:
+		print("Error: Invalid POST body")
         return
 
 
