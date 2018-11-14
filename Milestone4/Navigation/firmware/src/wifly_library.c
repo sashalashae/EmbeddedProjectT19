5a9e8ed093245dd0468fe2e3b77b549252a565e9
Thomas <tbellerose96@gmail.com>
2018-11-14 12:07:11 -0500

    Navigation bugs fixed for fully asynchronous functionality and robustness

--- Milestone4/Navigation/firmware/src/wifly_library.c
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Filename:    wifly_library.c
- * Date:        9/30/2018
- * Description: Implementation for a set of WiFly helper functions.
- * Author:      Alex Nolan
-*******************************************************************************/
-
-#include "wifly_library.h"
